9d4bff232bb69e8c3ca84d6c41ce6f0f8c76cd63
Tobias.Hein <tobias.hein@netresearch.de>
2017-09-20 10:39:54 +0200

    AR2-81: add service registration and eID call for clearcache service - make it possible to clear caches via URL curl in TYPO3 v8

--- ext_localconf.php
+++ ext_localconf.php
@@ -11,3 +11,28 @@
  */
 
 defined('TYPO3_MODE') or die('Access denied.');
+
+$TYPO3_CONF_VARS['FE']['eID_include'][$_EXTKEY]
+    = 'EXT:' . $_EXTKEY . '/eid/nr_sync.php';
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addService(
+    $_EXTKEY,
+    'nrClearCache' /* sv type */,
+    'tx_nrsync_clearcache' /* sv key */,
+    array(
+
+        'title' => 'NrSync Cache clear',
+        'description' => 'Clears the cache of given tables',
+
+        'subtype' => '',
+
+        'available' => true,
+        'priority' => 50,
+        'quality' => 50,
+
+        'os' => '',
+        'exec' => '',
+
+        'className' => Netresearch\Sync\Service\clearCache::class,
+    )
+);
